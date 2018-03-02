9adb5bf34a5ce92f074caf4efafa15a71a5b4036
Arne Brasseur <arne@arnebrasseur.net>
2018-03-02 13:41:36 +0800

    Basic cleanup
    Some code formatting changes
    
    - re-indent
    - avoid newlines inside functions
    - avoid dangling parens
    - with threading and .. macros, put the initial argument on the same line
    - require clojure.string :as str
    - swap a thread-last with a thread-first macro, so you can avoid the anonymous
      function.

--- src/us_energy_slopegraph/core.cljs
+++ src/us_energy_slopegraph/core.cljs
@@ -1,6 +1,7 @@
 (ns us-energy-slopegraph.core
   (:require-macros [cljs.core.async.macros :refer [go]])
-  (:require [cljsjs.d3]
+  (:require [clojure.string :as str]
+            [cljsjs.d3]
             [cljs-http.client :as http]
             [cljs.core.async :refer [<!]]))
 
@@ -11,12 +12,10 @@
 (def h 400)
 (def column-space 300)
 
-
-(defonce svg (..
-          (.select js/d3 "#slopegraph")
-          (append "svg")
-          (attr "height" h)
-          (attr "width" w)))
+(defonce svg (.. (.select js/d3 "#slopegraph")
+                 (append "svg")
+                 (attr "height" h)
+                 (attr "width" w)))
 
 (defonce height-scale
   (.. js/d3
@@ -25,23 +24,20 @@
       (range #js [(- h 15) 2])))
 
 (defn format-fuel-name [fuel-name]
-  (->>
-   (name fuel-name)
-   (clojure.string/capitalize)
-   (#(clojure.string/replace % #"-" " "))
-  ))
+  (-> (name fuel-name)
+      (str/capitalize)
+      (str/replace #"-" " ")))
 
 (defn draw-header [years]
-    (.. svg
-        (selectAll "text.slopegraph-header")
-        (data (into-array years))
-        (enter)
-        (append "text")
-        (classed "slopegraph-header" true)
-        (attr "x" (fn [d i] (+ 10 (* i column-space)) ))
-        (attr "y" 15)
-        (text #(str %))
-    ))
+  (.. svg
+      (selectAll "text.slopegraph-header")
+      (data (into-array years))
+      (enter)
+      (append "text")
+      (classed "slopegraph-header" true)
+      (attr "x" (fn [d i] (+ 10 (* i column-space)) ))
+      (attr "y" 15)
+      (text #(str %))))
 
 (defn column1 [data]
   (.. svg
@@ -68,9 +64,7 @@
       (text #(str
               ((.format js/d3 ".2%") (val %))
               " "
-              (format-fuel-name (key %))))
-      ))
-
+              (format-fuel-name (key %))))))
 
 (defn draw-line [data-col-1 data-col-2]
   (.. svg
@@ -82,40 +76,27 @@
       (attr "x1" 55)
       (attr "x2" (- column-space 5))
       (attr "y1" #(height-scale (val %)))
-      (attr "y2" #(height-scale ((key %) data-col-2)))
-      ))
-
+      (attr "y2" #(height-scale ((key %) data-col-2)))))
 
 (defn draw-slopegraph [data]
   (let [data-2005 (:values (first (filter #(= 2005 (:year %)) data)))
         data-2015 (:values (first (filter #(= 2015 (:year %)) data)))]
-
     (draw-header [2005 2015])
     (column1 data-2005)
     (column2 data-2015)
-    (draw-line data-2005 data-2015)
-    ))
+    (draw-line data-2005 data-2015)))
 
 ;; Drawing our slopegraph
 
 (def data
-  [{
-    :year 2005,
-    :values {
-             :natural-gas 0.2008611514256557,
-             :coal 0.48970650816857986,
-             :nuclear 0.19367190804075465
-             }
-    }
-
-   {
-    :year 2015,
-    :values {
-             :natural-gas 0.33808321253456974,
-             :coal 0.3039492492908485,
-             :nuclear 0.1976276775179704
-             }
-    }])
+  [{:year 2005
+    :values {:natural-gas 0.2008611514256557
+             :coal 0.48970650816857986
+             :nuclear 0.19367190804075465}}
+   {:year 2015
+    :values {:natural-gas 0.33808321253456974
+             :coal 0.3039492492908485
+             :nuclear 0.1976276775179704}}])
 
 (draw-slopegraph data)
 
